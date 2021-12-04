7941be76f9f53a3996d9439c798b3137e407d61b
Himanshu Pareek <himanshupareekiit01@gmail.com>
2021-12-04 23:27:08 +0530

    Fixes one mistake

--- AngularOAuth2Demo/src/app/config/authCodeFlowConfig.ts
+++ AngularOAuth2Demo/src/app/config/authCodeFlowConfig.ts
@@ -2,12 +2,12 @@ import { AuthConfig } from "angular-oauth2-oidc";
 
 // If you are using okta as authorization server
 // it will be https://<Your Okta Domain>/oauth2/default
-const ISSUER: string = "https://dev-20751509.okta.com/oauth2/default";
+const ISSUER: string = "<Your Issuer URI>";
 
 export const authCodeFlowConfig: AuthConfig = {
   issuer: ISSUER,
   redirectUri: window.location.origin,
-  clientId: "0oa2xnabuyoTLhTsI5d7",
+  clientId: "<Your Client Id>",
   responseType: "code",
   scope: "openid profile email",
   showDebugInformation: true
